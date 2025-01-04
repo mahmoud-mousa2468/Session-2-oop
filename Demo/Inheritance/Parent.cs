2d729d900072f678addbd14327c1cccc6be91cda
Mo <Mo@DESKTOP-7P4PPCV>
2025-01-04 15:53:29 +0200

    Inheritance

---
+++ Demo/Inheritance/Parent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Inheritance
+{
+    internal class Parent
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Fun01()
+        {
+            Console.WriteLine("Iam Parent [Base]");
+        }
+        public void Fun02()
+        {
+            Console.WriteLine($"X : {X} :: Y : {Y}");
+        }
+        public Parent(int X,int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public override string ToString()
+        {
+            return $"X : {X} :: Y : {Y}";
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Demo
+﻿using Demo.Inheritance;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Inheritance
+            //Parent parent = new Parent(1, 2);
+            ////Console.WriteLine(parent);
+            //parent.Fun01();
+            //parent.Fun02();
+
+            //Child child = new Child(1, 2, 3);
+            //child.Fun01();
+            //child.Fun02();
+            #endregion
         }
     }
 }
